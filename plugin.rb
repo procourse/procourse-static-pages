2e79401651054a6a15e3484d8a14b425445a127f
Justin DiRose <justin@justindirose.com>
2018-06-17 07:28:55 -0500

    FEATURE - Implemented onceoff Sidekiq migration job
    Job looks for any rows in the PluginStore with the old plugin name associated, migrates them to the new one, and cleans up the information from the old plugin.

--- plugin.rb
+++ plugin.rb
@@ -10,10 +10,16 @@ add_admin_route 'procourse_static_pages.title', 'procourse-static-pages'
 
 register_asset "stylesheets/pc-static-pages.scss"
 
-Discourse::Application.routes.append do
-  get '/admin/plugins/procourse-static-pages' => 'admin/plugins#index', constraints: StaffConstraint.new
-  get '/page/:slug/:id' => 'pc_static_pages/pages#show'
-  get '/page/:id' => 'pc_static_pages/pages#show'
-end
-
 load File.expand_path('../lib/pc_static_pages/engine.rb', __FILE__)
+
+after_initialize do
+
+    Discourse::Application.routes.append do
+      get '/admin/plugins/procourse-static-pages' => 'admin/plugins#index', constraints: StaffConstraint.new
+      get '/page/:slug/:id' => 'pc_static_pages/pages#show'
+      get '/page/:id' => 'pc_static_pages/pages#show'
+    end
+
+    load File.expand_path('../app/jobs/onceoff/migrate_static_pages_plugin.rb', __FILE__)
+
+end
